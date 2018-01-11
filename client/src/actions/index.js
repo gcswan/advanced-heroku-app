a4f375a11f40645ee38ffa98cbd8f02abe36903e
Greg Swan <greg.c.swan@gmail.com>
2018-01-10 20:12:44 -0600

    s

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,7 +1,7 @@
 export function loadSearch(searchTerm) {
     return function(dispatch){
         let apiKey = 'd201dfe99583c2d7fea063e8fcf9b4dd';
-        let fetchURL = 'http://api.themoviedb.org/3/search/movie?api_key=' + apiKey + '&language=en-US&query=' + searchTerm + '&page=1'
+        let fetchURL = 'https://api.themoviedb.org/3/search/movie?api_key=' + apiKey + '&language=en-US&query=' + searchTerm + '&page=1'
         let workingApiCall = "https://api.themoviedb.org/3/search/movie?api_key=d201dfe99583c2d7fea063e8fcf9b4dd&language=en-US&query=the%20lion%20king&page=1&include_adult=false"
         dispatch({
             type: "LOAD_SEARCH"
